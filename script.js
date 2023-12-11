ec20786ac7741d111b214216b4e81ae5b79a1378
Svorky <kalex92@yandex.ru>
2023-12-12 01:22:33 +0200

    fixes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-import data from './data.json' assert {type: 'json'}
-
 let board = document.getElementById("board")
 let limbo = document.getElementById("limbo")
 
-window.addEventListener("load", (event) => {
+window.addEventListener("load", async (event) => {
+    const response = await fetch("./data.json")
+    const data = await response.json()
     board.innerHTML = data.board.map(item => `<p>${item}</p>`).join('')
     board.innerHTML += "<h3>Чистилище</h3>"
     board.innerHTML += data.limbo.map(item => `<p>${item}</p>`).join('')
