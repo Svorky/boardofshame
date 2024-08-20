b5e08a0068d76a0cf3f6995e3574d3c1829b6f90
ommgl <52328312+ommgl@users.noreply.github.com>
2024-08-20 15:54:19 +0300

    обновление дизайна

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let board = document.getElementById("board")
-let limbo = document.getElementById("limbo")
-
+var i = 0;
+function style() {
+	let style1 = 'color: red; background-color: blue;'
+	let style2 = 'color: blue; background-color: red;'
+	if(i%2 == 0) document.body.style = style1;
+	else document.body.style = style2;
+	i++;
+}
 window.addEventListener("load", async (event) => {
-    const response = await fetch("./shame_data.json")
-    const data = await response.json()
-    board.innerHTML = data.board.map(item => `<p>${item}</p>`).join('')
-    board.innerHTML += "<h3>Чистилище</h3>"
-    board.innerHTML += data.limbo.map(item => `<p>${item}</p>`).join('')
-  });
+	const response = await fetch("./shame_data.json")
+	const data = await response.json()
+	let board = document.getElementById("board")
+	board.innerHTML = data.board.map(item => `<li>${item}`).join('')
+	board.innerHTML += "<h2>Чистилище</h2>"
+	board.innerHTML += data.limbo.map(item => `<li>${item}`).join('')
+	style();
+	setInterval(style, 3000);
+});
